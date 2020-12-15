5cd9e6f98c5f71eb127af59495be29fa1705657d
jerry <17481452@qq.com>
2020-12-15 17:20:34 +0800

    候试室纠正

--- tm-wx-web/grails-app/assets/javascripts/qualify-exam.js
+++ tm-wx-web/grails-app/assets/javascripts/qualify-exam.js
@@ -23,7 +23,7 @@ $('#confirm').on('click', function(event){
                             "<div> <label>考场号：</label><span>第" + data.roomId + "考场</span>\n" +
                             "<div> <label>考场教室：</label><span>" + data.roomName + "</span>\n" +
                             "<div> <label>考试科目：</label><span>" + data.type + "</span>\n" +
-                            "<div> <label>侯考室：</label><span>第" + data.roomId + "组</span>\n" +
+                            "<div> <label>候考室：</label><span>励耘楼A111 第" + data.roomId + "组</span>\n" +
                             "</div>";
                     } else {
                         html = "考生号和密码不匹配！注意身份证号末位是字母的，请用大写字母。";
